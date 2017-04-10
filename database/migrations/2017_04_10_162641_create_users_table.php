e10819778f885ac1f28d5335442e589a505a852b
Sanjit Singh <sanjitsingh@Sanjits-iMac.local>
2017-04-10 17:11:03 -0400

    1.2 Added users table migration

---
+++ database/migrations/2017_04_10_162641_create_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+
+
+        public function up()
+        {
+                Schema::create('users', function (Blueprint $table) {
+                        
+                        $table->increments('id');
+                        $table->string('email');
+                        $table->string('password');
+                        $table->string('first_name')->nullable();
+                        $table->string('last_name')->nullable();
+                        $table->string('location')->nullable();
+                        $table->string('remember_token')->nullable();
+                        $table->timestamps();
+
+                });
+        }
+
+        public function down()
+        {
+                Schema::drop('users');
+        }               
+}
